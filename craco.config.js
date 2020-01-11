5829a944410aa0f155df2c9528b9bec035e3ed1f
Alexander-Pomazan <57513298+Alexander-Pomazan@users.noreply.github.com>
2020-01-11 10:57:47 +0300

    add hot reload for dev environment (#2)

--- craco.config.js
+++ craco.config.js
@@ -1,16 +1,24 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const CopyPlugin = require('copy-webpack-plugin')
+const reactHotReloadPlugin = require('craco-plugin-react-hot-reload')
 
 module.exports = {
   eslint: {
-    enable: false
+    enable: false,
   },
   webpack: {
     plugins: [
       new CopyPlugin([
         { from: '*.worker.js', context: './node_modules/tdweb/dist/' },
         { from: '*.mem', context: './node_modules/tdweb/dist/' },
-        { from: '*.wasm', context: './node_modules/tdweb/dist/' }
-      ])
-    ]
-  }
+        { from: '*.wasm', context: './node_modules/tdweb/dist/' },
+      ]),
+    ],
+    alias: {
+      'react-dom': '@hot-loader/react-dom',
+    },
+  },
+  plugins: [{
+    plugin: reactHotReloadPlugin,
+  }],
 }
