2cc320e429a97a6ccbba79d1ee312ac0c12c6926
Alexander-Pomazan <alexander.s.pomazan@gmail.com>
2020-01-13 21:01:04 +0300

    add code form component

---
+++ src/code-form-container.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import { CodeForm } from './code-form'
+
+// eslint-disable-next-line no-console
+export const CodeFormContainer = () => <CodeForm onSubmit={console.log} />
